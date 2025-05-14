adac6fbc52c89d1a5aa8b4badf4b5dee66fd0445
Aadiyat-II <aadiyat.ahmad@outlook.com>
2025-05-14 15:56:32 -0400

    moved logic for arranging columns out of App and into ColumnWrapper (#2)

--- src/components/ColumnWrapper/ColumnWrapper.tsx
+++ src/components/ColumnWrapper/ColumnWrapper.tsx
@@ -1,6 +1,18 @@
 import type { ReactNode } from "react";
+import Column from "../Column/Column";
+
 import './ColumnWrapper.css';
 
-export default function ColumnWrapper({ children } : { children: ReactNode }){
-    return <div className="column-wrapper">{children}</div>
+export default function ColumnWrapper({ symbols  } : { symbols: ReactNode[] }){
+    const numCols = 2
+    const symbolsPerColumn = symbols.length/numCols
+
+    const columns = Array.from(
+        { length: numCols }, (_, i) => 
+          <Column 
+            colSymbols={symbols.slice(i*symbolsPerColumn, i*symbolsPerColumn + symbolsPerColumn)}
+          />
+    )
+          
+    return <div className="column-wrapper">{columns}</div>
 }
