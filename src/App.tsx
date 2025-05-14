68029f8fdfa91f98ed54a3e5f6e72273576fee52
Aadiyat Ahmad <aadiyat.ahmad@outlook.com>
2025-05-14 13:38:53 -0400

    Highlight symbols on mouse enter

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 import Column from './components/Column/Column'
 import ColumnWrapper from './components/ColumnWrapper/ColumnWrapper'
@@ -7,11 +8,14 @@ import { symbolArray } from './utils/symbolArray'
 function App() {
   const numCols = 2
   const symbolsPerColumn = symbolArray.length/numCols
-
-  const symbols = symbolArray.map((sym, i) => <Symbol symbol={sym} handleMouseEnter={()=>handleMouseEnterSymbol(i)}/>)
+  
+  const [ highlightedSymbols, setHighlightedSymbols ] = useState<string[]>(symbolArray.map((_, i) => ""))
+  const symbols = symbolArray.map((sym, i) => <Symbol  symbol={sym} handleMouseEnter={()=>handleMouseEnterSymbol(i)} className={highlightedSymbols[i]}/>)
 
   function handleMouseEnterSymbol(idx: number){
-    console.log(idx)
+    const newHighlightedSymbols = [...highlightedSymbols]
+    newHighlightedSymbols[idx] = "highlighted-symbol"
+    setHighlightedSymbols(newHighlightedSymbols)
   }
 
   const Columns = Array.from(
--- src/components/Symbol/Symbol.tsx
+++ src/components/Symbol/Symbol.tsx
@@ -1,3 +1,5 @@
-export default function Symbol( { symbol, handleMouseEnter } : { symbol: string, handleMouseEnter: CallableFunction }){
-    return <span onMouseEnter={()=>handleMouseEnter()}>{symbol}</span>
+import './Symbol.css'
+
+export default function Symbol( { symbol, handleMouseEnter, className } : { symbol: string, handleMouseEnter: CallableFunction, className: string}){
+    return <span onMouseEnter={()=>handleMouseEnter()} className={className}>{symbol}</span>
 }
