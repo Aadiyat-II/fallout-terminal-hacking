e5c0ba46384ce5564521965c79b6c108d14517e7
Aadiyat Ahmad <aadiyat.ahmad@outlook.com>
2025-05-19 14:11:55 -0400

    log clicking on individual characters

--- src/App.tsx
+++ src/App.tsx
@@ -21,36 +21,33 @@ function App() {
   const [ logMessages, setLogMessages ] = useState<string[]>([])
   const [ currentSelection, setCurrentSelection ] = useState<string>('|')
 
-  const symbols = symbolArray.map((sym, i) => <Character  
-    symbol={sym}
-    handleMouseEnter={()=>handleMouseEnterSymbol(i)}
-    handleMouseLeave={()=>handleMouseLeaveSymbol()}
-    handleClick={()=>handleClick(i)}
-    className={highlightedSymbols[i]}
-  />)
-
+  
   function handleClick(idx: number){
     if(remainingAttempts){
-      let word = isWord(idx)
+      let wordIdx = isWord(idx)
       let bracketPair = findCorrespondingBracketIfAny(idx);
 
-      if(word>-1){
-        checkGuess(word)
+      if(wordIdx>-1){
+        checkGuess(wordIdx)
       }
       else if(bracketPair.valid){
-        logMessages.push(bracketPair.selection)
+        logBracketSelection(bracketPair)
         giveReward()
         setBracketBlacklist([...bracketBlacklist, bracketPair.start, bracketPair.end])
       }
+      else{
+        logSymbolSelection()
+      }
     }
-
+  
+    
     function checkGuess(word: number) {
       const guess = selectedWords[word]
       const numMatches = compareStrings(password, guess)
       const nextLogMessages = [...logMessages]
       nextLogMessages.push(guess)
       nextLogMessages.push(`Likeness=${numMatches}`)
-
+      
       if(numMatches === wordLength){
         nextLogMessages.push("Access Granted!")
       }
@@ -58,10 +55,18 @@ function App() {
         setRemainingAttempts(remainingAttempts-1)
         nextLogMessages.push("Access Denied.")
       }
-
+      
       setLogMessages(nextLogMessages)
     }
-  
+    
+
+    function logBracketSelection(bracketPair: BracketPair) {
+      const nextLogMessages = [...logMessages]
+      nextLogMessages.push(bracketPair.selection)
+      setLogMessages(nextLogMessages)
+    }
+    
+
     function giveReward(){
       if(Math.random() < triesResetProbablity){
         resetTries()
@@ -71,11 +76,13 @@ function App() {
       }
     }
   
+
     function resetTries(){
       logMessages.push("Reset Tries!")
       setRemainingAttempts(totalTries)
     }
   
+
     function removeDud(){
       const duds = Array.from(selectedWords, (elem, i) => {
         if(elem != password)
@@ -100,15 +107,24 @@ function App() {
   
       setSymbolArray(nextSymbolArray)
     }
+
+
+    function logSymbolSelection() {
+      const nextLogMessages = [...logMessages]
+      nextLogMessages.push(symbolArray[idx])
+      nextLogMessages.push("ERROR")
+      setLogMessages(nextLogMessages)
+    }
   }
+
 
   function handleMouseEnterSymbol(idx: number){
     let nextHighlightedSymbols = Array.from(symbolArray, (_)=> "")
     
-    let word = isWord(idx)
+    let wordIdx = isWord(idx)
     let bracketPair = findCorrespondingBracketIfAny(idx);
 
-    if(word>-1){
+    if(wordIdx>-1){
       highlightWholeWord()
     }
     else if(bracketPair.valid){
@@ -120,13 +136,15 @@ function App() {
 
     setHighlightedSymbols(nextHighlightedSymbols)
 
+
     function highlightWholeWord() {
-      const wordStart = wordStartIndices[word]
+      const wordStart = wordStartIndices[wordIdx]
       for (let i = wordStart; i < wordStart + wordLength; i++) {
         nextHighlightedSymbols[i] = highlightedSymbolClassName
       }
       setCurrentSelection(symbolArray.slice(wordStart, wordStart+wordLength).join(''))
     }
+
 
     function highlightBracketPair() {
       for (let i = bracketPair.start; i <= bracketPair.end; i++) {
@@ -135,11 +153,13 @@ function App() {
       setCurrentSelection(bracketPair.selection)
     }
 
+
     function highlightSymbol() {
       nextHighlightedSymbols[idx] = highlightedSymbolClassName
       setCurrentSelection(symbolArray[idx])
     }
   }
+
 
   function handleMouseLeaveSymbol(){
     const nextHighlightedSymbols = Array.from(symbolArray, (_)=> "")
@@ -147,11 +167,13 @@ function App() {
     setCurrentSelection('|')
   }
 
+
   function isWord(i:number){
     // Compares the index of a symbol with the index of where each word starts (and ends) to see if the  symbol is part of a word
     return wordStartIndices.findIndex((element) => i >= element && i < element+wordLength)
   }
-
+  
+  
   function findCorrespondingBracketIfAny(selectedSymbolIdx: number){
     const openBrackets = ['<', '(', '{', '[']
     const closeBrackets = ['>', ')', '}', ']']
@@ -197,13 +219,23 @@ function App() {
       return loc
     }
 
+
     function hasWordBetween(start: number, end: number){
       return wordStartIndices.some((val)=>val >= start && val < end)
     }
   }
 
+  
+  const symbols = symbolArray.map((sym, i) => <Character  
+      symbol={sym}
+      handleMouseEnter={()=>handleMouseEnterSymbol(i)}
+      handleMouseLeave={()=>handleMouseLeaveSymbol()}
+      handleClick={()=>handleClick(i)}
+      className={highlightedSymbols[i]}
+    />
+  )
 
-return (
+  return (
     <>
       <RemainingAttempts remainingAttempts={remainingAttempts}/>
       <div className='game-board'>
