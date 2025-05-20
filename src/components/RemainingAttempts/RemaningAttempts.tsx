3b9660da1cd49239f8b19c341a85a5453238872c
Aadiyat-II <aadiyat.ahmad@outlook.com>
2025-05-20 18:39:40 -0400

    Deploy/build to gh pages (#7)
    * fix TS warning
    
    * update vite config base

--- src/components/RemainingAttempts/RemaningAttempts.tsx
+++ src/components/RemainingAttempts/RemaningAttempts.tsx
@@ -1,7 +1,7 @@
 import './RemainingAttempts.css'
 
 export default function RemainingAttempts({ remainingAttempts } : { remainingAttempts: number}){
-    return <div className="remaining-attempts">Attempt(s) Remaining: {Array.from({length:remainingAttempts}, (v, i)=><AttemptMarker/>)}</div>
+    return <div className="remaining-attempts">Attempt(s) Remaining: {Array.from({length:remainingAttempts}, ()=><AttemptMarker/>)}</div>
 }
 
 function AttemptMarker(){
