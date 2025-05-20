0fc7656e44e5849e7dc1d112fa39d90ed3c7ff01
Aadiyat Ahmad <aadiyat.ahmad@outlook.com>
2025-05-20 15:50:19 -0400

    increase number of words

--- src/utils/gameParameters.ts
+++ src/utils/gameParameters.ts
@@ -1,7 +1,7 @@
 export const candidateWords= ["dangers", "sending", "central", "hunters", "resides", "believe", "venture", "pattern", "discard", "mention", "cutters", "canteen", "beliefs", "banning", "minigun", "cistern"];
 export const miscSymbols = [',','.','!','@','#','$','%','&','(',')','{','}','[',']','<','>','?','"',"'", '/', '|'];
 export const symbolArrayLength = 384
-export const numWords = 10
+export const numWords = 12
 export const chunkLength = Math.floor(symbolArrayLength / numWords);
 
 export const triesResetProbablity = 0.2
